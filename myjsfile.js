245b153769c2683ca538bb76150dfad332a64ca0
reddy-nithin <reddy.nithin.0331@gmail.com>
2021-07-30 21:39:19 +0530

    Basic Files

---
+++ myjsfile.js
@@ -0,0 +1,30 @@
+const taskContainer = document.querySelector(".task_container");
+console.log(taskContainer);
+const saveChanges = () => {
+  const taskData = {
+    id: `${Date.now()}`,
+    imageUrl: document.getElementById('imageurl').value,
+    taskTitle: document.getElementById('tasktitle').value,
+    taskType: document.getElementById('tasktype').value,
+    taskDescription: document.getElementById('taskdescription').value
+  };
+
+  const newCard = `
+  <div class="col-sm-12 col-md-6 col-lg-4" id=${taskData.id}>
+    <div class="card">
+<div class="card-header d-flex justify-content-end gap-2">
+  <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+<button type="button" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
+</div>
+<div class="card-body">
+  <img class="card-img-top" src=${taskData.imageUrl} alt="card_image">
+  <h5 class="card-title text-primary fw-bolder mt-2">${taskData.taskTitle}</h5>
+  <p class="card-text">${taskData.taskDescription}</p>
+  <a href="#" class="btn btn-primary">${taskData.taskType}</a>
+</div>
+</div>
+  </div>
+  `;
+
+  taskContainer.insertAdjacentHTML("beforeend", newCard);
+};
